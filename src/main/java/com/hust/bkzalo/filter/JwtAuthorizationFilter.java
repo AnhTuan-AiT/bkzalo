56f1689b70ff6f086d73e36c0d8cc73be30d5cac
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-11-07 20:50:25 +0700

    [feature] add feature: sign up, login

--- src/main/java/com/hust/bkzalo/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/hust/bkzalo/filter/JwtAuthorizationFilter.java
@@ -2,7 +2,6 @@ package com.hust.bkzalo.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
@@ -22,7 +21,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.hust.bkzalo.filter.JwtTokenUtils.algorithm;
+import static com.hust.bkzalo.utils.JwtTokenUtils.algorithm;
 import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
